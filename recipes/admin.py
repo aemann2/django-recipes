8fbda4f14f0297cce5a77d3571fe247bf1158a5c
aemann2 <aemann2@gmail.com>
2022-06-18 17:47:36 -0700

    customized user admin

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,8 +1,21 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import User, Recipe, RecipeIngredient
+from .models import Recipe, RecipeIngredient, User
 
 # Register your models here.
-admin.site.register(User, UserAdmin)
-admin.site.register(Recipe)
+
+# a more sophisticated way to handle our data, using StackedInline and custom admin field ordering
+class RecipeIngredientInline(admin.StackedInline):
+    model = RecipeIngredient
+    fields = ["name", "quantity", "unit"]
+
+
+class RecipeAdmin(admin.ModelAdmin):
+    inlines = [RecipeIngredientInline]
+    list_display = ["name", "user"]
+    readonly_fields = ["timestamp", "updated"]
+
+
+admin.site.register(Recipe, RecipeAdmin)
 admin.site.register(RecipeIngredient)
+admin.site.register(User, UserAdmin)
