dfebd2f7f9d702cd46d44b1580a118847afd2947
aemann2 <aemann2@gmail.com>
2022-06-18 17:08:07 -0700

    created User model, ran black formatting

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import Recipe, RecipeIngredient
+from django.contrib.auth.admin import UserAdmin
+from .models import User, Recipe, RecipeIngredient
 
 # Register your models here.
+admin.site.register(User, UserAdmin)
 admin.site.register(Recipe)
 admin.site.register(RecipeIngredient)
--- recipes/models.py
+++ recipes/models.py
@@ -1,6 +1,14 @@
 from django.db import models
+from django.conf import settings
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+
+class User(AbstractUser):
+    username = models.CharField(max_length=30, unique=True)
+    email = models.EmailField(max_length=255, unique=True)
+    password = models.CharField(max_length=200)
 
 
 class Recipe(models.Model):
@@ -10,6 +18,10 @@ class Recipe(models.Model):
     timestamp = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     active = models.BooleanField(default=True)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
 
 
 class RecipeIngredient(models.Model):
